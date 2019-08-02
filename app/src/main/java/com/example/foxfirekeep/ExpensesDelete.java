79c774e1e8f2059ed168e02c8eb79bb34a4d8775
Charin <kcdperera50000@gmail.com>
2019-08-02 23:19:38 +0530

    ExpensesCrudUpdate2

---
+++ app/src/main/java/com/example/foxfirekeep/ExpensesDelete.java
@@ -0,0 +1,14 @@
+package com.example.foxfirekeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExpensesDelete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expenses_delete);
+    }
+}
