667518aa7371b9e07617f93bfa9fea69afedd5d0
PC <IT18122138@my.sliit.lk>
2019-08-03 00:45:00 +0530

    Uplode

---
+++ app/src/main/java/com/example/foxfirekeep/Forum_IUD.java
@@ -0,0 +1,14 @@
+package com.example.foxfirekeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Forum_IUD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forum__iud);
+    }
+}
