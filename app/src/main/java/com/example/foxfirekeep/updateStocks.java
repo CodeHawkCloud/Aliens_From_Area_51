d39808dfb0fb0154ba0e85a57ca7c4b80524fe65
SupunRajitha <supunrajitha16@gmail.com>
2019-08-02 22:22:36 +0530

    update Stock

---
+++ app/src/main/java/com/example/foxfirekeep/updateStocks.java
@@ -0,0 +1,14 @@
+package com.example.foxfirekeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class updateStocks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_stocks);
+    }
+}
