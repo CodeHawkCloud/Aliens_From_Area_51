293657535a06edd92b1de6b63d1175e2278d7342
HP <rajinthacorera@gmail.com>
2019-08-03 11:31:03 +0530

    7th

---
+++ app/src/main/java/com/example/foxfirekeep/inventoryCrudMenu.java
@@ -0,0 +1,14 @@
+package com.example.foxfirekeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inventoryCrudMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_crud_menu);
+    }
+}
