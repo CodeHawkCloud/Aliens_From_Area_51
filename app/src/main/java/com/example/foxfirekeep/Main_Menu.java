d18bc43e95a30b84ce1af702b0e0193e18893c75
HP <rajinthacorera@gmail.com>
2019-08-05 00:21:56 +0530

    23nd

--- app/src/main/java/com/example/foxfirekeep/Main_Menu.java
+++ app/src/main/java/com/example/foxfirekeep/Main_Menu.java
@@ -2,7 +2,10 @@ package com.example.foxfirekeep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main_Menu extends AppCompatActivity {
 
@@ -10,5 +13,53 @@ public class Main_Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__menu);
+
+        Button b1 = (Button)findViewById(R.id.buttonExpen);
+        Button b2 = (Button)findViewById(R.id.buttonSale);
+        Button b3 = (Button)findViewById(R.id.button_inventory_main);
+        Button b4 = (Button)findViewById(R.id.buttonForum);
+        Button b5 = (Button)findViewById(R.id.buttonBusi);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it1 = new Intent(Main_Menu.this,ExpensesCrudMenu.class);
+                startActivity(it1);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it2 = new Intent(Main_Menu.this,SalesCrudMenu.class);
+                startActivity(it2);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it3 = new Intent(Main_Menu.this,inventoryCrudMenu.class);
+                startActivity(it3);
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it4 = new Intent(Main_Menu.this,ForumCrudMenu.class);
+                startActivity(it4);
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it5 = new Intent(Main_Menu.this,MyBusiness.class);
+                startActivity(it5);
+            }
+        });
+
     }
 }
+
