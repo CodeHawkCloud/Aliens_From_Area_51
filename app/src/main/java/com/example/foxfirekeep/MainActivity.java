8c4d70764bedec646f2e8111183efd61223b97f3
HP <rajinthacorera@gmail.com>
2019-08-04 16:24:43 +0530

    20th

--- app/src/main/java/com/example/foxfirekeep/MainActivity.java
+++ app/src/main/java/com/example/foxfirekeep/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.foxfirekeep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    //splash time interval
+    private int splash_time = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent mmIntent = new Intent(MainActivity.this,Main_Menu.class);
+                startActivity(mmIntent);
+                finish();
+            }
+        },splash_time);
     }
 }
