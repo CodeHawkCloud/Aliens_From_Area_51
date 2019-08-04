4cb5a2e31b5cd8b951f899c0672378252d3f7ec6
HP <rajinthacorera@gmail.com>
2019-08-05 01:27:00 +0530

    24th

--- app/src/main/java/com/example/foxfirekeep/SalesCrudMenu.java
+++ app/src/main/java/com/example/foxfirekeep/SalesCrudMenu.java
@@ -2,18 +2,30 @@ package com.example.foxfirekeep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ScaleDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class SalesCrudMenu extends AppCompatActivity {
+    private ImageView home1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sales_crud_menu);
 
+        home1 = (ImageView)findViewById(R.id.home);
 
+        home1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it1 = new Intent(SalesCrudMenu.this,Main_Menu.class);
+                startActivity(it1);
+            }
+        });
     }
 }
