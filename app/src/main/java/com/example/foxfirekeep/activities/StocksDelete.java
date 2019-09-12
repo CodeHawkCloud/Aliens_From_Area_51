957e87d1492a835a94b3de5aa5fc7f3844d4ef00
SupunRajitha <supunrajitha16@gmail.com>
2019-09-12 23:22:54 +0530

    ^_^

--- app/src/main/java/com/example/foxfirekeep/activities/StocksDelete.java
+++ app/src/main/java/com/example/foxfirekeep/activities/StocksDelete.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.foxfirekeep.activities.R;
+import com.example.foxfirekeep.database.DBHandler;
 
 public class StocksDelete extends AppCompatActivity {
-    private ImageView back;
+    ImageView back; //variable for the back button
+    EditText eStocksDeleteId; //variable for the edit text
+    Button stocksDeleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stocks_delete);
+
+        /*---------------back button [START]---------------*/
 
         back = (ImageView)findViewById(R.id.button_back);
 
@@ -26,5 +36,67 @@ public class StocksDelete extends AppCompatActivity {
                 startActivity(it1);
             }
         });
+
+        /*---------------back button [END]-----------------*/
+
+        /*---------------insert crud operation 1st part [START]---------------*/
+
+        //assigning id value to the variables using the ids of the EditText
+        eStocksDeleteId = findViewById(R.id.edit_stocks_delete_name);
+
+        //assigning delete button
+        stocksDeleteButton = (Button)findViewById(R.id.button_stocks_delete_delete);
+
+        //calling the TextWatcher function to the id field
+        eStocksDeleteId.addTextChangedListener(deleteStocksTextWatcher);
+
+        /*---------------insert crud operation 1st part [END]-----------------*/
     }
+
+    /*---------------insert crud operation 2nd part [START]---------------*/
+    public void onClick(View view){
+
+        int onStocksDeleteId = Integer.parseInt(eStocksDeleteId.getText().toString());
+
+        //DBHandler object created
+        DBHandler dbhandler = new DBHandler(this);
+
+        //Toast creation
+        Toast t;
+
+        //check if the insertion was successful
+        if(dbhandler.deleteStocks(onStocksDeleteId)){
+            //Toast message if insertion is successful
+            t = Toast.makeText(getApplicationContext(),"Stock has been deleted from FoxFire!", Toast.LENGTH_LONG);
+            t.show();
+        }
+        else{
+            //Toast message if insertion fails
+            t = Toast.makeText(getApplicationContext(),"Stock deletion failed!", Toast.LENGTH_LONG);
+            t.show();
+        }
+    }
+
+    /*---------------insert crud operation 2nd part [END]-----------------*/
+
+    //TextWatcher function
+    private TextWatcher deleteStocksTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            int onStocksDeleteId = Integer.parseInt(eStocksDeleteId.getText().toString().trim());
+
+            stocksDeleteButton.setEnabled(onStocksDeleteId >0);
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 }
