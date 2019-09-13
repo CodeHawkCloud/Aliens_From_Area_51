360c91409e5e9c17861878d40b026c6229cf0651
Charin <kcdperera50000@gmail.com>
2019-09-13 13:00:03 +0530

    Expenses Update

--- app/src/main/java/com/example/foxfirekeep/activities/ExpensesDelete.java
+++ app/src/main/java/com/example/foxfirekeep/activities/ExpensesDelete.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.foxfirekeep.activities.R;
+import com.example.foxfirekeep.database.DBHandler;
 
 public class ExpensesDelete extends AppCompatActivity {
     private ImageView back;
+    EditText eIdInput;
+    Button deleteButton;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +35,49 @@ public class ExpensesDelete extends AppCompatActivity {
                 startActivity(it1);
             }
         });
+
+        eIdInput = findViewById(R.id.edit_expenses_delete_id);
+
+        deleteButton = (Button)findViewById(R.id.button_expenses_delete_delete);
+
+        eIdInput.addTextChangedListener(deleteExpenseTextWatcher);
     }
+
+    public void onClick(View view){
+        int eId = Integer.parseInt(eIdInput.getText().toString());
+
+        Toast toast;
+
+        DBHandler dbHandler = new DBHandler(this);
+
+        if(dbHandler.deleteExpenditure(eId)){
+            toast = Toast.makeText(getApplicationContext(),"Expense has been deleted from FoxFire!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else {
+            toast = Toast.makeText(getApplicationContext(),"Expense has not been deleted from FoxFire!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+    //Text watcher method implementation
+    private TextWatcher deleteExpenseTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            String onExpenseDeleteId = eIdInput.getText().toString().trim();
+
+            deleteButton.setEnabled(!onExpenseDeleteId.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 }
