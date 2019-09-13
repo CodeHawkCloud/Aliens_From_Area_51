613ade10898432ee48792c308680fc237d64d064
Charin <kcdperera50000@gmail.com>
2019-09-13 13:39:55 +0530

    Expenses Update

--- app/src/main/java/com/example/foxfirekeep/adapters/ExpensesAdapter.java
+++ app/src/main/java/com/example/foxfirekeep/adapters/ExpensesAdapter.java
@@ -36,32 +36,6 @@ public class ExpensesAdapter extends ArrayAdapter {
         return l3.get(position);
     }
 
-    /*View row = convertView;
-        SalesHolder salesHolder = new SalesHolder();
-
-        if(row == null) {
-            LayoutInflater lf = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row = lf.inflate(R.layout.activity_sales_view, parent, false);
-
-            salesHolder.sales_holder_id = (TextView)row.findViewById(R.id.text_sales_view_col_name_id);
-            salesHolder.sales_holder_item = (TextView)row.findViewById(R.id.text_sales_view_col_name_item);
-            salesHolder.sales_holder_brand = (TextView)row.findViewById(R.id.text_sales_view_col_name_brand);
-            salesHolder.sales_holder_price = (TextView)row.findViewById(R.id.text_sales_view_col_name_price);
-            salesHolder.sales_holder_quantity = (TextView)row.findViewById(R.id.text_sales_view_col_name_quantity);
-            row.setTag(salesHolder);
-        }
-        else{
-            salesHolder = (SalesHolder) row.getTag();
-        }
-
-        Sales sales = (Sales)getItem(position);
-        salesHolder.sales_holder_id.setText(Integer.toString(sales.getSales_id()));
-        salesHolder.sales_holder_item.setText(sales.getSalesItem().toString());
-        salesHolder.sales_holder_brand.setText(sales.getSalesBrand().toString());
-        salesHolder.sales_holder_price.setText(Integer.toString(sales.getSalesPrice()));
-        salesHolder.sales_holder_quantity.setText(Integer.toString(sales.getSalesQuantity()));
-
-        return row;*/
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         View row = convertView;
